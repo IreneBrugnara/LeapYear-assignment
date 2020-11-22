90d5d63ed2baa9144539da24e72dd2180b1e1a3a
Irene Brugnara <irene.brugnara@leader.it>
2020-11-22 21:09:54 +0100

    remove code duplication

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -9,13 +9,17 @@ public class LeapYear {
     public Boolean isLeap(int year) {
         if(year%4 != 0)
             return false;
-        else if(year%100 != 0 && year%400 != 0)
-            return true;
-        else if(year%100 == 0 && year%400 != 0)
-            return false;
-        else if(year%100 == 0 && year%400 == 0)
-            return true;
-        else
-            return null;
+        else {
+            if(year%100 == 0) {
+                if (year % 400 == 0)
+                    return true;
+                else
+                    return false;
+            }
+            else if(year%100 != 0 && year%400 != 0)
+                return true;
+            else
+                return null;
+        }
     }
 }
